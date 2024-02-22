e945d477973890b68fa0b61144ed0fabe528bd44
Nathaniel Nurlegn <nathaniel.an17@gmail.com>
2024-02-22 14:02:08 -0500

    Modified operate() in funcs to output a tuple containing a number list and a operation list. Create a some unit tests? or pytests? currently works but will be modified to fit pytests or unit tests.

--- funcs.py
+++ funcs.py
@@ -4,18 +4,74 @@ from decimal import Decimal, getcontext
 getcontext()
 
 def Operate(statement):
-    slist = list(statement)
-    ans = "Error"
-    for i in range(len(slist)):
-        if slist[i] in valid_ops:
-            num1 = "".join(slist[0:i])
-            num2 = "".join(slist[i+1:len(slist)])
-            try:
-                ans = valid_ops[slist[i]](num1,num2)
-            except ValueError:
-                return "Invalid statement was made, please enter a proper statement"
-            break
-    return ans
+
+    temp = list(statement.replace(" ", "")) # Returns a list ex: ['2','+','3','-','4']
+    ind = 0
+    numberQueue = []
+    opQueue = []
+    numToAdd = ""
+    start = 0
+
+    if not temp[ind].isdigit(): #or not (temp[ind] != '-' and temp[ind+1].isnumeric()):
+        return "Error improper statement"
+    else:
+        if temp[ind] == "-" and not temp[ind+1].isdigit():
+            return "Error improper statement"
+        else:
+            if temp[ind] in valid_ops:
+                return "Error improper statement"
+
+    while ind < len(temp):
+        if temp[ind] in valid_ops and not (temp[ind-1] in valid_ops):
+                num = "".join(temp[start:ind])
+                print(num)
+                start = ind + 1
+                numberQueue.append(num)
+                opQueue.append(temp[ind])
+        if temp[ind] in "-" and temp[ind-1] in valid_ops:
+            start = ind
+        ind += 1
+    num = "".join(temp[start:ind])
+    numberQueue.append(num)
+    return numberQueue, opQueue
+
+    # slist = rmSpace(list(statement))
+    # if not slist[0].isnumeric(): return "Error"
+    # nums = []
+    # ops = []
+    # prev = 0
+    # print(slist)
+    # for i in range(len(slist)): # 2+-4-5+-2 
+    #     if slist[i] in valid_ops:
+    #         if slist[i] == "-" and slist[i-1] in valid_ops:
+    #             continue
+    #         nums.append("".join(slist[prev:i]))
+    #         ops.insert(0,slist[i])
+    #         prev = i+1
+    # # nums is the list of numbers (as strings) and ops is the queue of operations
+    # num1 = None
+    # num2 = None
+    # ans = None
+    # print(nums)
+    # print(ops)
+    # for i in range(len(nums)): # 2+3+4+5
+    #     if num1 == None:
+    #         num1 = nums[i]
+    #         continue
+    #     if num2 == None:
+    #         num2 = nums[i]
+    #         continue
+    #     op = ops.pop()
+    #     if ans == None:
+    #         ans = valid_ops[op](num1,num2)
+    #     val = nums[i]
+    #     ans = valid_ops[op](str(ans),val)
+    # return ans
+        
+         
+    
+
+
 
 def add(a,b):
     return str(Decimal(a) + Decimal(b))
---
+++ test_funcs.py
@@ -0,0 +1,25 @@
+# testing functions in funcs.py
+import pytest
+from funcs import *
+
+def test_Operate_numList():
+    test_string = "25 + 3  -   4"
+    test_Output = ['25','+','3','-','4']
+    numQ = Operate(test_string)[0]
+    if numQ is str:
+        print("Function is reading improper input")
+    numCheck = ["25","3","4"]
+    print(numQ)
+    assert numQ == numCheck
+
+def test_Operate_opList():
+    test_string = "25 + 3  -   4"
+    test_Output = ['25','+','3','-','4']
+    opQ = Operate(test_string)[1]
+    opCheck = ["+","-"]
+    print(opQ)
+    assert opQ == opCheck
+
+
+    
+    
